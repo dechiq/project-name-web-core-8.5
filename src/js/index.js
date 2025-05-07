1b2130598054612066022d7fb78134a4ef7b1744
dechiq <tenkirill04@gmail.com>
2025-05-07 17:17:16 +0900

    final

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,20 @@
+import '../scss/index.scss'
 import '../scss/style.scss'
-import '../scss/adaptive.css'
-import '../scss/index.css'
+import '../scss/adaptive.scss'
+
 import './index2.js'
 import './index3.js'
 import './index4.js'
+
+if (window.innerWidth <= 767) {
+    let swiper = new Swiper('.mySwiper', {
+      slidesPerView: 'auto',
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      }
+    })
+  }
 console.log('It works!')
